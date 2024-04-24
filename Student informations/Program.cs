536d993152245f10b1acb8e008d76ca2631031f0
Xabibullayev Kamronbek <simplekux8082@icloud.com>
2024-04-24 21:32:38 +0500

    Add project files.

---
+++ Student informations/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student student = new Student();
+        Console.WriteLine("Please enter your name :");
+        student.Name = Console.ReadLine();
+        Console.WriteLine("Then enter your ID :");
+        student.Id = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your informations :");
+        Console.WriteLine(student.Name + "  " + student.Id);
+}
+}
+ class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
